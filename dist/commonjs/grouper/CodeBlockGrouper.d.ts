53e31ea9c75e4ccc1a1389f2e9cef47ffcb78c36
Welkin Wong <welkin.wong@me.com>
2025-10-14 11:46:20 +0800

    chore: remove dist directory from version control

--- dist/commonjs/grouper/CodeBlockGrouper.d.ts
+++
@@ -1,5 +0,0 @@
-import { TDataGroup } from './group-types';
-export declare class CodeBlockGrouper {
-  group(groups: TDataGroup[]): TDataGroup[];
-  private convertCodeBlocksToCodeBlockGroups;
-}
--- dist/commonjs/grouper/ListGrouper.d.ts
+++
@@ -1,5 +0,0 @@
-import { TDataGroup } from './group-types';
-export declare class ListGrouper {
-  group(groups: TDataGroup[]): TDataGroup[];
-  private convertListBlocksToListGroups;
-}
